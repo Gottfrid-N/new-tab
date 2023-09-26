7be3ee0034e2b6dd654dd718a02eb1c8c9f029e8
Gottfrid-N <gottfrid@familjen-nordstrom.se>
2023-09-26 19:57:57 +0200

    goofy ahhhhhh

---
+++ scripts/quick-link-nav.js
@@ -0,0 +1,9 @@
+var navbar = document.querySelector("#ql-nav");
+navbar.append("    <nav>");
+navbar.append("        <ul>");
+navbar.append("            <li><a href=\"personal.html\">Personal Quick Links</a></li>");
+navbar.append("            <li><a href=\"school.html\">School Quick Links</a></li>");
+navbar.append("        </ul>");
+navbar.append("    </nav>");
+
+navbar.innerHTML.
